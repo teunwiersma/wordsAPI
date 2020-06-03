6ac7421a80619b36489c0d949e6b6bc5cdc26ad1
Teun Wiermsa <Teun.Wiersma@student.hu.nl>
2020-06-03 21:12:50 +0200

    properties  update 5

--- src/main/kotlin/com/REST/WordAPI/DAO/WordDaoImpl.kt
+++ src/main/kotlin/com/REST/WordAPI/DAO/WordDaoImpl.kt
@@ -1,28 +1,32 @@
 package com.REST.WordAPI.DAO
 
-import com.REST.WordAPI.Mapper.WordMapper
 import com.REST.WordAPI.Model.Word
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.jdbc.core.namedparam.SqlParameterSource
 import org.springframework.jdbc.support.GeneratedKeyHolder
 import org.springframework.jdbc.support.KeyHolder
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
 
 
 @Repository
 class WordDaoImpl : WordDao {
 
+    @Autowired
+    private var template: NamedParameterJdbcTemplate? = null
 
     fun WordDaoImpl(template: NamedParameterJdbcTemplate?) {
         this.template = template
     }
 
-    var template: NamedParameterJdbcTemplate? = null
-
+    var rowMapper: RowMapper<Word> = RowMapper<Word> { resultSet: ResultSet, rowIndex: Int ->
+        Word(resultSet.getInt("id"), resultSet.getString("word") )}
 
     override fun findAll(): List<Word?>? {
-        return template?.query("select * from word", WordMapper())
+        return template?.query("select * from word", rowMapper)
     }
 
     override fun insertWord(w: Word?) {
