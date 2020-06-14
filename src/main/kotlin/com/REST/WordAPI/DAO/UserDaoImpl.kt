4dc4af4174ad0baaf7a708ad9ca853fb6095e679
Teun Wiermsa <Teun.Wiersma@student.hu.nl>
2020-06-14 16:15:04 +0200

    model update

--- src/main/kotlin/com/REST/WordAPI/DAO/UserDaoImpl.kt
+++ src/main/kotlin/com/REST/WordAPI/DAO/UserDaoImpl.kt
@@ -18,18 +18,17 @@ class UserDaoImpl : UserDao {
     private var template: NamedParameterJdbcTemplate? = null
 
     var rowMapper: RowMapper<User> = RowMapper<User> { resultSet: ResultSet, rowIndex: Int ->
-        User(resultSet.getInt("userid"), resultSet.getString("username"), resultSet.getString("password"), resultSet.getInt("highscore"))
+        User( resultSet.getString("username"),resultSet.getInt("highscore"))
     }
 
     override fun findAll(): List<User?>? {
-        return template?.query("select username, highscore from users limit 5", rowMapper)
+        return template?.query("select * from users limit 5", rowMapper)
     }
 
     override fun insertUser(user: User) {
-        val sql = "insert into users (username, password, highscore) values(:username, :password, :highscore)"
+        val sql = "insert into users (username, highscore) values(:username, :highscore)"
         val holder: KeyHolder = GeneratedKeyHolder()
         val param: SqlParameterSource = MapSqlParameterSource().addValue("username", user.getUserName())
-                .addValue("password", user.getPassword())
                 .addValue("highscore", user.getHighscore())
         template!!.update(sql, param, holder)
     }
--- src/main/kotlin/com/REST/WordAPI/Domain/User.kt
+++ src/main/kotlin/com/REST/WordAPI/Domain/User.kt
@@ -1,14 +1,7 @@
 package com.REST.WordAPI.Domain
 
-class User (private var userid: Int, private var username: String, private var password: String, private var highscore: Int){
+class User ( private var username: String, private var highscore: Int){
 
-    fun getId(): Int? {
-        return userid
-    }
-
-    fun setId(userid: Int){
-        this.userid = userid
-    }
 
     fun getUserName(): String? {
         return username
@@ -16,14 +9,6 @@ class User (private var userid: Int, private var username: String, private var p
 
     fun setUserName(username: String){
         this.username = username
-    }
-
-    fun getPassword(): String? {
-        return password
-    }
-
-    fun setPassword(password: String){
-        this.password = password
     }
 
     fun getHighscore(): Int? {
