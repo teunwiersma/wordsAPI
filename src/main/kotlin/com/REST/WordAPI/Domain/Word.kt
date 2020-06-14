1c8ac0e4636c488d0cf7bb8a5fb21df205c27e53
Teun Wiermsa <Teun.Wiersma@student.hu.nl>
2020-06-14 19:49:10 +0200

    constructor fix

--- src/main/kotlin/com/REST/WordAPI/Domain/Word.kt
+++ src/main/kotlin/com/REST/WordAPI/Domain/Word.kt
@@ -5,9 +5,13 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 @JsonDeserialize
 class Word constructor(private var word: String){
 
+    fun Word(){
+
+    }
     fun getWord(): String? {
         return word
     }
+
 
     fun setWord(word: String) {
         this.word = word
