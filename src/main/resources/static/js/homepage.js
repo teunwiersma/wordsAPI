0cc2fc32dbfb04368236f3da5e4086691f320947
teunwiersma <31851921+teunwiersma@users.noreply.github.com>
2020-06-14 21:08:26 +0200

    Update homepage.js

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -19,7 +19,6 @@ async function getWords(){
 
 function gameloop(){
     const word = wordList[Math.floor(Math.random() * wordList.length)].toUpperCase();
-    console.log(word);
     const hasDuplicates = (/([a-zA-Z]).*?\1/).test(word);
     const wordLength = word.length
     beurtNummer = 1;
